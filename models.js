32350803d2203048a85ad5f3cf088f42093b0bce
Nam Ninja <namninja@gmail.com>
2019-02-22 18:48:20 -0800

    Completed building blog model and schema

---
+++ models.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+// this is our schema to represent a restaurant
+const blogSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String },
+  author: {
+    firstName: String,
+    // coord will be an array of string values
+    lastName: String
+  },
+  // grades will be an array of objects
+  created: { type: Date, default: Date.now }
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the address object
+// we're storing in Mongo.
+blogSchema.virtual("authorName").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName,
+    created: this.created
+  };
+};
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = { Blog };
